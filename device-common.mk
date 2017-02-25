300c5405c411053676f54179e9b469ac03fb2675
Martin Bouchet <tincho5588@gmail.com>
2017-02-25 15:52:27 +0530

    smdk5260: Switch to the new audio HAL in hardware/samsung
    Change-Id: I230f6433b6d891fa3bead301af93501f9963c384
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -21,7 +21,8 @@ PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
