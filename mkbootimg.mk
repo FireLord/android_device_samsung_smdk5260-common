4741f8d5e11404e21eff755cc10a145db6219f61
FireLord1 <firelord.xda@gmail.com>
2017-03-17 02:36:01 +0530

    smdk5260: try to compress the recovery ramdisk with lzma
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,5 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
+LZMA_RAMDISK := $(PRODUCT_OUT)/ramdisk-recovery-lzma.img
+
+$(LZMA_RAMDISK): $(recovery_ramdisk)
+	gunzip -f < $(recovery_ramdisk) | lzma > $@
+
 ## Overload bootimg generation: Same as the original, + BUMP
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	$(call pretty,"Target boot image: $@")
@@ -9,9 +14,9 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	@echo "Made boot image: $@"
 
 ## Overload recoveryimg generation: Same as the original, + BUMP
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(LZMA_RAMDISK) $(recovery_kernel)
 	$(call build-recoveryimage-target, $@)
-	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --ramdisk $(LZMA_RAMDISK)
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
 	@echo "SEANDROIDENFORCE" >> $@
 	@echo "Made recovery image: $@"
